edf995370e1e9b5bac181ce4d8263b4a4980f7df
Nazaret <nazaretgutirrezmartn@gmail.com>
2025-11-13 21:22:48 +0100

    ejercicio-cuentaBank

--- CuentaBancaria.java
+++ CuentaBancaria.java
@@ -4,21 +4,40 @@ public class CuentaBancaria {
    private String titular;
 
 
-   public CuentaBancaria(String numeroCuenta, double saldo, String titular) {
+    public CuentaBancaria(String numeroCuenta, double saldo, String titular) {
         this.numeroCuenta = numeroCuenta;
         this.saldo = saldo;
         this.titular = titular;
    }
 
-   public getNumeroCuenta() {
-    System.out.println("Introduce tu número de cuenta");
+    public void depositar(double cantidad) {
+        if (cantidad <= 0) {
+            System.out.println("No tienes nada para ingresar.");
+            return;
+        }
+        this.saldo += cantidad;
+        System.out.println("Has depositado un total de " + cantidad + " euros.");
    }
 
-   public extraccion() {
-        if (this.saldo < 0) {
-            System.out.println("Estás a 0, no puedes sacar dinero");
-        } else {
-            System.out.println("Has sacado %f", this.saldo);
-        }
+    public void extraer(double cantidad) {
+       if (this.saldo <= 0) {
+           System.out.println("Estás a 0, no puedes sacar dinero");
+       } else if (cantidad > this.saldo) {
+           System.out.println("No puedes sacar más dinero del que tienes actualmente");
+       } else {
+           this.saldo -= cantidad;
+           System.out.println("Has sacado un total de " + cantidad + " euros.");
+       }
    }
+
+    public double consultarSaldo() {
+        return this.saldo;
+    }
+
+    public void datosDeCuenta() {
+        System.out.println("Titular: " + this.titular);
+        System.out.println("Número de cuenta: " + this.numeroCuenta);
+        System.out.println("Saldo: " + this.saldo);
+    }
+
 }
