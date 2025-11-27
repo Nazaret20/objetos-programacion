d7dab5e9c04019672c5895f40dc162bd69bebc53
Nazaret <nazaretgutirrezmartn@gmail.com>
2025-11-27 10:23:01 +0100

    ejercicios 27N

--- ascensor/src/AppAscensor.java
+++ ascensor/src/AppAscensor.java
@@ -1,49 +1,95 @@
 import java.util.Scanner;
-
 public class AppAscensor {
-    public static void menuAscensor() {
-        System.out.println("______________________________________________");
-        System.out.println("=== HOTEL GRAN VÍA - SISTEMA DE ASCENSOR ===");
-        System.out.println("\nEdificio: 12 pisos (0-11)");
-        System.out.println("1. Llamar ascensor a mi piso");
-        System.out.println("2. Ir a un piso específico");
-        System.out.println("3. Abrir/Cerrar puerta manualmente");
-        System.out.println("4. Ver estado del ascensor");
-        System.out.println("5. EMERGENCIA");
-        System.out.println("0. Salir\n");
-        System.out.print("¿Qué necesitas hacer?: ");
-    }
     public static void main(String[] args) throws Exception {
-        Ascensor ascensor = new Ascensor(11);
         Scanner sc = new Scanner(System.in);
+        Ascensor asc = new Ascensor(11);
+        int opc;
+        boolean emergencia = false;
 
-        int opc, pisoUser;
-        
-        System.out.println("Bienvenido al ascensor de Hotel Gran Vía\n");
+        System.out.println("\nBienvenid@ al Hotel Gran Vía\n");
         do {
-            menuAscensor();
-            opc = sc.nextInt();
+            menu();
+            if(!emergencia)
+                opc = Integer.parseInt(sc.nextLine());
+            else 
+                opc = 6;
 
             switch (opc) {
                 case 1:
-                    System.out.print("¿En qué piso estás del 0 al 11?: ");
-                    pisoUser = sc.nextInt();
-                    System.out.println("El ascensor está en la planta" + ascensor.getPisoActual());
-                    
-                    if (pisoUser > ascensor.getPisoActual() || pisoUser > ascensor.getPisoActual()) {
-                        ascensor.irAPiso(pisoUser);
-                    }
+                    System.out.print("Dime el piso en el que estás: ");
+                    int piso1 = Integer.parseInt(sc.nextLine());
+                    asc.irAPiso(piso1);
+                    asc.abrirPuerta();
                     System.out.println("El ascensor ha llegado a su piso");
-                    
                     break;
-            
-                    case 4:
-                        ascensor.verificarEstado();
-                default:
+                case 2:
+                    System.out.print("Dime el piso al que vas: ");
+                    int piso2 = Integer.parseInt(sc.nextLine());
+                    asc.irAPiso(piso2);
+                    asc.abrirPuerta();
+                    System.out.printf("Ha llegado al piso %d. ¡Que tenga un buen día!", piso2);
                     break;
+                case 3:
+                    System.out.println("a) Abrir puerta");
+                    System.out.println("b) Cerrar puerta");
+                    System.out.print("\tSelecione opción: ");
+                    if(sc.nextLine().equals("a")) {
+                        if(asc.abrirPuerta())
+                            System.out.println("Puerta abierta");
+                        else {
+                            if(asc.isEnMovimiento())
+                                System.out.println("Fallo al abrir puerta.\nAscensor en movimiento.");
+                            else
+                                System.out.println("Fallo al abrir puerta.\nPuerta ya abierta.");
+                        }
+                    }
+                    else {
+                        if(asc.cerrarPuerta())
+                            System.out.println("Puerta cerrada");
+                        else
+                            System.out.println("Fallo al cerrar puerta. \nPuerta ya cerrada.");
+                    }                       
+                    break;
+                case 4:
+                    asc.verificarEstado();
+                    for(int i = asc.getPisoMaximo(); i >= 0; i--) {
+                        if(i != asc.getPisoActual())
+                            System.out.println("Piso " + i + ":");
+                        else
+                            System.out.println("Piso " + i + ": [X] <- Ascensor aquí");
+                    }
+                    break;
+                case 5:
+                    System.out.print("¿Está seguro de activar la emergencia? (s/n) ");
+                    if(sc.nextLine().equals("s")) {
+                        asc.activarEmergencia();
+                        System.out.println("Salga del ascensor y use las escaleras");
+                        emergencia = true;
+                    }
+                    break;
+                case 6:
+                    System.out.println("EN EMERGENCIA. LLAME AL SERVICIO TÉCNICO.");
+                    while (true) {
+                        
+                    }
             }
-        } while (opc != 0);
 
+        } while (opc != 0);
+        System.out.println("Finalizado correctamente");
         sc.close();
+    }
+
+    public static void menu() {
+        System.out.println("\n=== HOTEL GRAN VÍA - SISTEMA DE ASCENSOR ===\r\n" + //
+                        "Edificio: 12 pisos (0-11)\r\n" + //
+                        "\r\n" + //
+                        "1. Llamar ascensor a mi piso\r\n" + //
+                        "2. Ir a un piso específico\r\n" + //
+                        "3. Abrir/Cerrar puerta manualmente\r\n" + //
+                        "4. Ver estado del ascensor\r\n" + //
+                        "5. EMERGENCIA\r\n" + //
+                        "0. Salir\r\n" + //
+                        "============================================");
+                        System.out.print("\tSeleccione opción: ");
     }
 }
--- ascensor/src/Ascensor.java
+++ ascensor/src/Ascensor.java
@@ -1,98 +1,24 @@
 public class Ascensor {
+    //Atributos
     private int pisoActual, pisoMaximo;
     private boolean enMovimiento, puertaAbierta;
 
-    public Ascensor() {
+    //Constructores
+    Ascensor() {
         this.pisoActual = 0;
         this.pisoMaximo = 10;
         this.enMovimiento = false;
         this.puertaAbierta = false;
     }
 
-    public Ascensor(int pisoMaximo) {
+    Ascensor(int pisoMaximo) {
         this.pisoActual = 0;
         this.pisoMaximo = pisoMaximo;
         this.enMovimiento = false;
         this.puertaAbierta = false;
     }
 
-    // Métodos propios
-    public boolean subirPiso() {
-        if (this.pisoActual == this.pisoMaximo && this.puertaAbierta == true) {
-            this.enMovimiento = false;
-        }
-
-        if (this.enMovimiento == false) {
-            this.enMovimiento = true;
-            this.pisoActual++;
-            return this.enMovimiento = false;
-        } else {
-            return this.enMovimiento = true;
-        }
-    }
-
-    public boolean bajarPiso() {
-        if (this.pisoActual < 0 && this.puertaAbierta == true) {
-            this.enMovimiento = false;
-        } 
-
-        if (this.enMovimiento == false) {
-            this.enMovimiento = true;
-            this.pisoActual--;
-            return this.enMovimiento = false;
-        } else {
-            return this.enMovimiento = true;
-        }
-    }
-
-    public boolean abrirPuerta() {
-        if (this.enMovimiento == true && this.puertaAbierta == true) {
-            return false;  
-        } else {
-            return true;
-        } 
-    }
-
-    public boolean cerrarPuerta() {
-        if (this.puertaAbierta == true) {
-            return this.puertaAbierta = false;
-        } else {
-            return this.puertaAbierta = true;
-        } 
-    }
-
-    public boolean irAPiso(int pisoDestino) {
-        if (pisoDestino >= 0) {
-            subirPiso();
-        } 
-        
-        if (pisoDestino <= this.pisoMaximo) {
-            bajarPiso();
-        }
-
-        if (cerrarPuerta() == false) {
-            return true;
-        } else {
-            return false;
-        }  
-    }
-
-    public void activarEmergencia() {
-        this.enMovimiento = false;
-        abrirPuerta();
-        System.out.println("EMERGENCIA en piso: " + getPisoActual());
-
-    }
-
-    public void verificarEstado() {
-        System.out.println("Piso actual: " + getPisoActual());
-        System.out.println("La puerta está: " + isPuertaAbierta());
-        System.out.println("El movimiento es: " + isEnMovimiento());
-        System.out.println("Piso máximo: " + getPisoActual());
-
-    }
-
-    // Getters
+    //Getters
     public int getPisoActual() {
         return pisoActual;
     }
@@ -107,5 +33,87 @@ public class Ascensor {
 
     public boolean isPuertaAbierta() {
         return puertaAbierta;
+    }
+
+    //Métodos propios
+    public boolean subirPiso() {
+        if(!puertaAbierta && pisoActual < pisoMaximo) {
+            enMovimiento = true;
+            pisoActual++;
+            enMovimiento = false;
+            return true;
+        }
+        else
+            return false;
+    }
+    
+    public boolean bajarPiso() {
+        if(!puertaAbierta && pisoActual > 0) {
+            enMovimiento = true;
+            pisoActual--;
+            enMovimiento = false;
+            return true;
+        }
+        else
+            return false;
+    }
+
+    public boolean abrirPuerta() {
+        if(enMovimiento || puertaAbierta)
+            return false;
+        else {
+            puertaAbierta = true;
+            return true;
+        }
+    }
+
+    public boolean cerrarPuerta() {
+        if(!puertaAbierta)
+            return false;
+        else {
+            puertaAbierta = false;
+            return true;
+        }
+    }
+
+    public boolean irAPiso(int pisoDestino) {
+        if(pisoDestino < 0 || pisoDestino > pisoMaximo || pisoActual == pisoDestino) {
+            return false;
+        }
+        else {
+            cerrarPuerta();
+            if(pisoDestino > pisoActual) {
+                for(int piso = pisoActual; piso < pisoDestino; piso++) {
+                    subirPiso();
+                    System.out.print(piso + " -> ");
+                }
+            }
+            else {
+                for(int piso = pisoActual; piso > pisoDestino; piso--) {
+                    bajarPiso();
+                    System.out.print(piso + " -> ");
+                }
+            }
+            return true;
+        }
+    }
+
+    public void activarEmergencia() {
+        enMovimiento = false;
+        abrirPuerta();
+        System.out.println("EMERGENCIA EN PISO " + pisoActual);
+    }
+
+    public void verificarEstado() {
+        System.out.println("Piso Actual:\t" + pisoActual);
+        if(puertaAbierta)
+            System.out.println("Puerta:\t\tAbierta" );
+        else
+            System.out.println("Puerta:\t\tCerrada" );
+        if(enMovimiento)
+            System.out.println("Estado:\t\tEn movimiento" );
+        else
+            System.out.println("Puerta:\t\tDetenido" );
+        System.out.println("Piso Máximo:\t" + pisoMaximo);
     }
 }
